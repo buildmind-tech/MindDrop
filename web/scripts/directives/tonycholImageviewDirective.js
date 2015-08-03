9a854a0bf9d9a1ae665f0ad3f56ac2d858027f0e
TonyChol <tonycai1992@gmail.com>
2015-08-03 12:01:06 +0800

    Try upload file onto Server

--- web/scripts/directives/tonycholImageviewDirective.js
+++ web/scripts/directives/tonycholImageviewDirective.js
@@ -1,4 +1,4 @@
-angular.module('tonychol.imageviewDirective', [])
+angular.module('tonychol.imageviewDirective', ['ngFileUpload'])
     .factory('dropFilesService', [function() {
         var dropInstance = [{
             suffix: 'png',
@@ -68,7 +68,7 @@ angular.module('tonychol.imageviewDirective', [])
         };
         return directive;
     })
-    .controller('TonyCholDropFileCtrl', ['$scope', '$mdSidenav', 'dropFilesService', function($scope, $mdSidenav, dropFilesService) {
+    .controller('TonyCholDropFileCtrl', ['$scope', '$mdSidenav', 'dropFilesService','Upload', '$timeout', function($scope, $mdSidenav, dropFilesService, Upload, $timeout) {
         $scope.toggleSidenav = function(menuId) {
             $mdSidenav(menuId).toggle();
         };
@@ -77,4 +77,36 @@ angular.module('tonychol.imageviewDirective', [])
             address: '1600 Amphitheatre',
         };
         $scope.dropFiles = dropFilesService.dropFiles;
+        $scope.$watch('files', function() {
+            $scope.upload($scope.files);
+        });
+        $scope.$watch('file', function() {
+            $scope.upload([$scope.file]);
+        });
+        $scope.log = '';
+        $scope.upload = function(files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    Upload.upload({
+                        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                        fields: {
+                            'username': $scope.username
+                        },
+                        file: file
+                    }).progress(function(evt) {
+                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                        $scope.log = 'progress: ' + progressPercentage + '% ' +
+                            evt.config.file.name + '\n' + $scope.log;
+                    }).success(function(data, status, headers, config) {
+                        $timeout(function() {
+                            $scope.log = 'file: ' + config.file.name + ', Response: ' + JSON.stringify(data) + '\n' + $scope.log;
+                        });
+                    });
+                }
+            }
+        };
+
+
+
     }]);
